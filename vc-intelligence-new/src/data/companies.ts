fb8b94f26e3096c73535fab1427f7258c3863d96
Roza Getachew <getachewroza2@gmail.com>
2026-02-26 09:47:38 +0300

    added a mock file containing companies names and websites .

---
+++ vc-intelligence-new/src/data/companies.ts
@@ -0,0 +1,23 @@
+export const companies = [
+    {
+        id: "1",
+        name: "Stripe",
+        website: "https://stripe.com",
+        industry: "Fintech",
+        location: "USA"
+    },
+    {
+        id: "2",
+        name: "Notion",
+        website: "https://notion.so",
+        industry: "Productivity",
+        location: "USA"
+    },
+    {
+        id: "3",
+        name: "Flutterwave",
+        website: "https://flutterwave.com",
+        industry: "Fintech",
+        location: "Africa"
+    }
+];
